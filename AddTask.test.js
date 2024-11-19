864f92a364c92a0c1c04ed33b54c0a2bfaf61034
tulasivarre <99002904+tulasivarre@users.noreply.github.com>
2024-11-19 15:18:11 +0530

    AddTask.test.js
    Unit Testing

---
+++ AddTask.test.js
@@ -0,0 +1,15 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import AddTask from "../main/AddTask";
+
+test("allows user to add a task", () => {
+    const mockAdd = jest.fn();
+    render(<AddTask onAdd={mockAdd} />);
+
+    const input = screen.getByPlaceholderText("Add a task");
+    const button = screen.getByText("Add");
+
+    fireEvent.change(input, { target: { value: "New Task" } });
+    fireEvent.click(button);
+
+    expect(mockAdd).toHaveBeenCalledWith("New Task");
+});
